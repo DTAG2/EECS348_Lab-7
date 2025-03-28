6dc5546e0c7bb2acf9fe35ef09e2404049b59ebe
DTAG2 <143109081+DTAG2@users.noreply.github.com>
2025-03-27 23:35:37 -0500

    comments

--- football_main.c
+++ football_main.c
@@ -5,14 +5,14 @@ int main(int argc, char *argv[]) {
 
     int points;
 
-    while(1){
-    printf("Enter 0 or 1 to STOP\n");
-    printf("Enter the NFL score: ");
-    scanf("%d", &points);
-    if (points < 2){
-        break;
+    while(1){ //loop indefinetly
+    printf("Enter 0 or 1 to STOP\n"); 
+    printf("Enter the NFL score: "); //get user input
+    scanf("%d", &points); //obtain the user input
+    if (points < 2){ //if the score is less the 2 
+        break; //break the loop
     }
-    print_combinations(points);
+    print_combinations(points); //print the cominations of scoring
     }
     return 0;
 }
